5c0119fd0ec71f1eb9cdbb858351182bcf76a9a3
Thy <samgvilla@gmail.com>
2025-05-26 21:46:23 +0200

    Borra Chatgpt

---
+++ juego/doodlejump/test/TestPersonaje.java
@@ -0,0 +1,12 @@
+package doodlejump.test;
+
+import doodlejump.model.JuegoPersonaje;
+
+public class TestPersonaje {
+    public static void main(String[] args) {
+        JuegoPersonaje juego = new JuegoPersonaje();
+        juego.jugar();
+    }
+}
+
+
