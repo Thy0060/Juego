72311c516bc4b3fe24fa6c0f79912b2e62db6e0c
MDETMATCH <miguelangeljjimenezz@gmail.com>
2025-05-26 11:22:14 +0200

    testpersonaje arreglado

--- juego/doodlejump/model/TestPersonaje.java
+++ juego/doodlejump/model/TestPersonaje.java
@@ -1,9 +1,5 @@
 package doodlejump.model;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 56c1f24b5a37431e5bd29261825fb309cd05616f
 public class TestPersonaje {
     public static void main(String[] args) {
         JuegoPersonaje juego = new JuegoPersonaje();
