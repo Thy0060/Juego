a95a43d7433bf3ae8b6bcfd73fd384ec8a62a748
h <h>
2025-05-26 12:31:08 +0200

    Miguel angle gay

--- juego/doodlejump/model/PlataformaFalsa.java
+++ juego/doodlejump/model/PlataformaFalsa.java
@@ -1,18 +1,69 @@
 package doodlejump.model;
 
 import stdlib.StdDraw;
+
+import java.util.Arrays;
+
 import model.ObjetoGrafico;
+import model.ObjetoGraficoMovil;
+import model.base.IFigura;
+import model.base.Poligono;
+import model.base.Punto;
+
 public class PlataformaFalsa extends Plataforma{
+    protected boolean activa = true;
+    protected boolean colision = false;
+
     public PlataformaFalsa(double x, double y, Personaje personajeRef) {
         super(x, y, personajeRef);
-        getFigura().setColor(StdDraw.LIGHT_GRAY);
+        this.getFigura().setColor(StdDraw.LIGHT_GRAY);
+    }
+
+    private static IFigura crearFigura(double x, double y) {
+        return new Poligono(java.awt.Color.GREEN, new Punto(x, y),
+            Arrays.asList(
+                new Punto(x - ANCHO/2, y - ALTO/2),
+                new Punto(x + ANCHO/2, y - ALTO/2),
+                new Punto(x + ANCHO/2, y + ALTO/2),
+                new Punto(x - ANCHO/2, y + ALTO/2)
+            ));
+    }
+
+    public void verificarColision() {
+        if (activa && colisionaCon(personajeRef)) {
+            activa = false;
+        }
+    }
+
+     public boolean estaActiva() {
+        return activa;
+    }
+   
+    private boolean colisionaCon(Personaje pj) {
+    
+    Punto centroPlataforma = ((Poligono) this.getFigura()).getCentroide();
+    Punto centroPersonaje = ((Poligono) pj.getFigura()).getCentroide();
+
+    double x = centroPlataforma.getX();
+    double y = centroPlataforma.getY();
+
+    double px = centroPersonaje.getX();
+    double py = centroPersonaje.getY();
+
+    double izquierda = x - Plataforma.ANCHO / 2;
+    double derecha = x + Plataforma.ANCHO / 2;
+    double abajo = y - Plataforma.ALTO / 2;
+    double arriba = y + Plataforma.ALTO / 2;
+
+    return (px >= izquierda && px <= derecha && py >= abajo && py <= arriba);
+}
+     @Override
+    public void recibirImpacto(ObjetoGrafico f) {
+        verificarColision();
     }
 
     @Override
-    public void recibirImpacto(ObjetoGrafico otraFigura) {
-        if(activa && otraFigura instanceof Personaje) {
-            activa = false;
-            getFigura().setColor(new Color(0,0,0,0)); // Hacer transparente
-        }
+    public void mover(double incX, double incY) {
+        this.colocar(incX, incY);
     }
 }
